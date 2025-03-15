7dc1f93c226e82913f0806b9b56c57a6a0a0114a
arontandi <tandiaron@gmail.com>
2025-03-15 18:07:34 +0100

    refractor backend and frontend

--- frontend/app/core/services/openai.service.ts
+++ frontend/app/core/services/openai.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { OpenAI } from 'openai';
 import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
